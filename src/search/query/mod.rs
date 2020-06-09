672812859e7cb3d1622688c52c8854930388c268
William Myers <mwilliammyers@gmail.com>
2020-06-09 17:55:13 -0600

    Move query.types modules to query

--- src/search/query/mod.rs
+++ src/search/query/mod.rs
@@ -6,9 +6,20 @@ use std::default::Default;
 
 use serde::{Deserialize, Serialize};
 
-pub use self::types::*;
+pub use self::{
+    exists::*, match_::*, nested::*, query_string::*, range::*, regexp::*, simple_query_string::*,
+    term::*, terms::*,
+};
 
-mod types;
+mod exists;
+mod match_;
+mod nested;
+mod query_string;
+mod range;
+mod regexp;
+mod simple_query_string;
+mod term;
+mod terms;
 
 // TODO: make this file smaller!
 
